8ce34a9628e2b8d8b3c43f1cd95c47c9838dc9e3
John-Mark Gurney <jmg@funkthat.com>
2016-02-22 00:12:02 -0800

    add module metadata, including license..
    [git-p4: depot-paths = "//depot/python/pypasn1/main/": change = 1836]

--- pasn1.py
+++ pasn1.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
 
-# A Pure Python ASN.1 encoder/decoder w/ a calling interface in the spirit
-# of pickle.
-#
-# It uses a profile of ASN.1.
-#
-# All lengths must be specified.  That is that End-of-contents octets
-# MUST not be used.  The shorted form of length encoding MUST be used.
-# A longer length encoding MUST be rejected.
+'''A Pure Python ASN.1 encoder/decoder w/ a calling interface in the spirit
+of pickle.
+
+It uses a profile of ASN.1.
+
+All lengths must be specified.  That is that End-of-contents octets
+MUST not be used.  The shorted form of length encoding MUST be used.
+A longer length encoding MUST be rejected.'''
+
+__author__ = 'John-Mark Gurney'
+__copyright__ = 'Copyright 2016 John-Mark Gurney.  All rights reserved.'
+__license__ = '2-clause BSD license'
+
+# Copyright 2016, John-Mark Gurney
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# The views and conclusions contained in the software and documentation are those
+# of the authors and should not be interpreted as representing official policies,
+# either expressed or implied, of the Project.
 
 import datetime
 import math
