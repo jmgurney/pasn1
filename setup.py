ba1de73e8576a42dadd69ec24674202419ea63ab
John-Mark Gurney <jmg@funkthat.com>
2017-03-29 00:04:38 -0700

    make this a proper python module now..

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='pasn1',
+      version='1.0',
+      description='An ASN.1 encoder/decoder with json like interface.',
+      author='John-Mark Gurney',
+      author_email='jmg@funkthat.com',
+      url='https://github.com/jmgurney/pasn1',
+      py_modules=['pasn1'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+     )
